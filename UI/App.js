5835951fcbff40ce5453ed6b89c98901aed9f1ce
Viren Mirchandani <47482093+07virmir@users.noreply.github.com>
2022-01-29 19:55:02 -0500

    Add files via upload

---
+++ UI/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+	from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Upload from './pages/upload';
+
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/dashboard' element={<Dashboard/>} />
+		<Route path='/upload' element={<Upload/>} />
+	</Routes>
+	</Router>
+);
+}
+
+export default App;
+
---
+++ UI/dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => {
+return (
+	<h1>Main info for inventory analytics.</h1>
+);
+};
+
+export default Dashboard;
