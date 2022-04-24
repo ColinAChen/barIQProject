b3689c1fa9c9907e6f3bb8bf40845e7e05acdb1d
Colin Chen <colin.a.chen@gmail.com>
2022-04-24 16:53:55 -0400

    finalize segment, hook it up to app

---
+++ count/app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+#import detect
+import segment
+UPLOAD_FOLDER = './../data/samples'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+DISPLAY_FOLDER = './../output'
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DISPLAY_FOLDER'] = DISPLAY_FOLDER
+
+bqDict = {}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def demo_form():
+    print('render')
+    return render_template('demo.html')
+
+@app.route('/', methods=['GET', 'POST'])
+#def hello_world():  # put application's code here
+#    #print(os.chdir())
+#    return render_template('demo.html')#'Hello World!'
+
+
+#https://stackoverflow.com/questions/44926465/upload-image-in-flask
+def upload_file():
+    # maybe clear the folder before running inference?
+    if request.method == 'POST':
+        print("POST method reached")
+        # check if the post request has the file part
+        if 'files[]' not in request.files:
+            print('first')
+            flash('No file part')
+            return redirect(request.url)
+        files = request.files.getlist('files[]')
+        #print(files[0].filename)
+        file_names = []
+        for file in files:
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            #if file.filename == '':
+            #    flash('No selected file')
+            #    return redirect(request.url)
+            print(file.filename)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                # tell the webapp to always look for .png
+                saveName = filename.split('.')[0] + '.png'
+                # let the file system save the files as whatever, YOLO will handle file reading
+                file_names.append(saveName)
+                print('filename:', filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                #return redirect(request.url)
+                
+                # call the inference function
+                #detect.run()
+                
+                # outputs are now in the output folder
+                #retPath = os.path.join(app.config['DISPLAY_FOLDER'], filename)
+        #detect.run()
+        # call stuff here
+        bqDict = segment.evalPath()
+        return render_template('demo.html', filenames=file_names)
+        # display the html
+
+        #displayFile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return redirect(request.url)
+    #return render_template('demo.html', user_image=displayFile)    
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    #filename = filename.split('.')[0] + '.png'
+    #print("fetch:", filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+if __name__ == '__main__':
+    #print(os.listdir())
+    
+    app.run()
--- count/segment.py
+++ count/segment.py
@@ -1,9 +1,16 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-
+#import matplotlib.pyplot as plt
+import math
 from pyzbar.pyzbar import decode
+import os
 
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred, {'databaseURL':'https://bariq-jia1356-default-rtdb.firebaseio.com/'})
+import json
 '''
 try rough image segmentation based on flood fill
 
@@ -18,7 +25,8 @@ GREEN = (119, 171, 92)
 TARGET = GREEN #(207, 227, 228)#(215, 232, 232)#(183, 212, 232)#(1,0,122)#(215, 232, 232)
 THRESHOLD = 20
 COUNT_THRESHOLD = 1000
-PATH = 'hallway.jpg'#'aisle1.jpeg'#'barcode.jpg'#'chess.jpg'#'red.jpeg'
+PATH = 'gap4' 
+
 def main():
 
     ## j is start
@@ -42,11 +50,34 @@ def main():
     #plt.show()
     b,g,r = TARGET
     print(b/g, b/r, g/r)
+    #sendToDB(None)
+    
+    for path in os.listdir(PATH):
+        if 'green' in path or 'bq' in path or 'groups' in path:
+            #print('skip: ', path)
+            continue
+        
+        imagePath = os.path.join(PATH, path)
+        #print(imagePath)
+        #image = cv2.imread(imagePath)
+        #green = segmentGreen(image)
+        evalImage(imagePath)
+        #saveName = path.split('.')[0] + '_green.png'
+        #savePath = os.path.join(PATH, saveName)
+        #cv2.imwrite(savePath, green)
+        #green = cv2.resize(green, (500, 500))
+        #showImage(green.astype(np.uint8))
+        #detectRect(green)
 
-
-
-    #print(distance((1,2,3), (1,2,4)))
-    image = cv2.imread(PATH)
+    '''
+Find barcodes and count tags
+'''
+def evalImage(path):
+    image = cv2.imread(path)
+    green = segmentGreen(image)
+    #showGreen = cv2.resize(green, (500, 500))
+    #showImage(showGreen.astype(np.uint8))
+    #detectRect(green)
     # test sim color
 
     #g = cv2.imread(PATH, 0)
@@ -62,7 +93,7 @@ def main():
     #plt.plot(x,y)
     #plt.show()
     
-    
+    '''
     rows, cols, c = image.shape
     out = np.zeros(image.shape)
     for r in range(rows):
@@ -81,16 +112,30 @@ def main():
             else:
                 out[r][c] = (1,1,1)
     cv2.imwrite('simRatio.png', out*600)
-    
+    '''
     
     #g = cv2.imread('barcode.jpg', cv2.IMREAD_GRAYSCALE)
-    g = cv2.imread(PATH, cv2.IMREAD_GRAYSCALE)
-    ret, thresh = cv2.threshold(g, 127, 255, cv2.THRESH_BINARY)
+    color = cv2.imread(path)
+
+    g = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    bcDict, bq = barcodeSearch(path)
+    if len(bcDict.keys()) > 0:
+        saveName = path.split('.')[0] + '_bq.png'
+        cv2.imwrite(saveName, bq)
+    '''
+    ret, thresh = cv2.threshold(green.astype(np.uint8), 50, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(g, 50, 255, cv2.THRESH_BINARY)
+
+    showThresh = cv2.resize(thresh, (1000,1000))
+    showImage(showThresh)
     cv2.imwrite('gray.png', thresh)
     # decode barcode info
+    #decodeList = decode(thresh)
     decodeList = decode(thresh)
+
     print('num barcodes: ', len(decodeList))
-    parseBarcodeList(decodeList)
+    nameSet = parseBarcodeList(decodeList)
+    
     for barcode in decodeList: 
 
         # Locate the barcode position in image
@@ -98,12 +143,12 @@ def main():
          
         # Put the rectangle in image using
         # cv2 to heighlight the barcode
+        cv2.rectangle(color, (x-10, y-10),
+                      (x + w+10, y + h+10),
+                      (0, 0, 255), 7)
         #cv2.rectangle(thresh, (x-10, y-10),
-        #              (x + w+10, y + h+10),
-        #              (255, 0, 0), 2)
-        cv2.rectangle(thresh, (x-10, y-10),
-                  (x + w+10, y + h+10),
-                  0, 2)
+        #          (x + w+10, y + h+10),
+        #          0, 2)
 
 
         if barcode.data!="":
@@ -111,10 +156,12 @@ def main():
             # Print the barcode data
             print(barcode.data)
             print(barcode.type)
-    cv2.imwrite('detect.png', thresh)
+    
+    print('brands detected: ', nameSet)
+    cv2.imwrite('detect.png', color)
     image = cv2.resize(image, (100,100))
     img = cv2.imread('aisle1.jpeg', -1)
-    
+    '''
 
     #normalize(img)
     
@@ -124,22 +171,33 @@ def main():
     #for seg in segments:
     
     #mask = preprocess(image)
-    #groups = group(mask)
+    #showImage(green)
+    saveName = path.split('.')[0] + '_green.png'
+    cv2.imwrite(saveName, green)
+    mask = np.where(green>100, 255,0)
+    #showImage(mask.astype(np.uint8))
+    groups = group(mask.astype(np.uint8))
+    cThresh = 200
+    out = np.zeros(green.shape)
+    tags = countGroups(groups,thresh=cThresh, out=out)
+    #print('num tags:', tags)
     
+    #showImage(out)
+    saveName = path.split('.')[0] + '_groups.png'
+    cv2.imwrite(saveName, out) 
+    outDict = {}
+    if (len(bcDict.keys()) == 0):
+        outDict['none'] = tags
+    else:
+        split = len(bcDict.keys())
+        for b in bcDict:
+            outDict[b] = int(tags/split)
+
     #count = 0
     #out = np.zeros(mask.shape)
-    #for g in groups:
-    #    # do more refined counting
-    #    # if we can expect that mask will return squares we can use line detection or something to count the number of rectangles
-    #    if len(groups[g]) > COUNT_THRESHOLD:
-    #        count += 1
-    #        for p in groups[g]:
-    #            r,c = p
-    #            out[r][c] = 255
-    #cv2.imwrite('count.png', out)
-    #print('flags counted: ', count)
     
-
+    
+    '''
     #numFlags = count(image)
     
     out*=600
@@ -147,6 +205,11 @@ def main():
     #cv2.imwrite('over.png', out)
     numFlags = count(out, target=(0,0,0))
     print('flags of color', TARGET, ': ', numFlags)
+
+    '''
+    print(outDict)
+    return outDict
+
 def count(image, target=None):
     mask = preprocess(image, target)
     groups = group(mask)
@@ -212,7 +275,21 @@ def group(image):
                     queue.extend(n)
             group += 1
     return groups
-
+def countGroups(groups, thresh=50, out=None):
+    count = 0
+    for g in groups:
+        # do more refined counting
+        # if we can expect that mask will return squares we can use line detection or something to count the number of rectangles
+        if len(groups[g]) > thresh and checkSquare(groups[g]):
+            #checkSquare(groups[g])
+            count += 1
+            if out is not None:
+                for p in groups[g]:
+                    r,c = p
+                    out[r][c] = 255
+    #cv2.imwrite('count.png', out)
+    #print('flags counted: ', count)
+    return count
 '''
 Flood fill for color
 '''
@@ -348,11 +425,226 @@ def normalize(img):
     cv2.imwrite('shadows_out.png', result)
     cv2.imwrite('shadows_out_norm.png', result_norm)
 
-def barcode(image):
+def barcode(image, draw=False):
+    
     pass
-def parseBarcodeList(bq):
-    for b in bq:
-        print(b)
 
+
+'''
+Try multiple threshold values to find any barcodes in the image
+'''
+def barcodeSearch(path):
+    color = cv2.imread(path)
+
+    g = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    print(path)
+    totalList = []
+    totalSet = set()
+    bcDict = {}
+    for t in range(10, 240, 3):
+        ret, thresh = cv2.threshold(g, t, 255, cv2.THRESH_BINARY)
+        decodeList = decode(thresh)
+        for barcode in decodeList:
+            d = barcode.data
+            r = barcode.rect
+            if d in bcDict:
+                bcDict[d].append(r)
+            else:
+                bcDict[d] = [r]
+            #totalList.append(barcode.rect)
+            #totalSet.add(barcode.
+        #print('num barcodes: ', len(decodeList))
+        #nameSet = parseBarcodeList(decodeList)
+        #print('t: ',t, 'nameSet: ', nameSet)
+        
+        continue
+        if (len(nameSet) == 0):
+            continue
+        
+        for barcode in decodeList: 
+
+            # Locate the barcode position in image
+            (x, y, w, h) = barcode.rect
+             
+            # Put the rectangle in image using
+            # cv2 to heighlight the barcode
+            cv2.rectangle(color, (x-10, y-10),
+                          (x + w+10, y + h+10),
+                          (0, 0, 255), 7)
+            #cv2.rectangle(thresh, (x-10, y-10),
+            #          (x + w+10, y + h+10),
+            #          0, 2)
+
+
+            if barcode.data!="":
+               
+                # Print the barcode data
+                print(barcode.data)
+                print(barcode.type)
+        
+        #print('brands detected: ', nameSet)
+        #cv2.imwrite('detect.png', color)
+        #image = cv2.resize(thresh, (100,100))
+        #cv2.showImage(image)
+    #print(bcDict)
+    c = [(255,0,0), (0,0,255)]
+    for i, bc in enumerate(bcDict):
+        for rect in bcDict[bc]:
+            (x, y, w, h) = rect
+             
+            # Put the rectangle in image using
+            # cv2 to heighlight the barcode
+            cv2.rectangle(color, (x-10, y-10),
+                          (x + w+10, y + h+10),
+                          c[i], 7)
+    #cv2.imwrite('detect.png', color)
+    return bcDict, color
+def parseBarcodeList(bq):
+    nameSet = set()
+    for b in bq:
+        #print(b.data)
+        nameSet.add(b.data)
+        #print(b)
+    return nameSet
+'''
+Send data to the database
+
+https://www.freecodecamp.org/news/how-to-get-started-with-firebase-using-python/
+'''
+def sendToDB(counts):
+    ref = db.reference("/")
+    test = {"Michelob Ultra": 4} 
+    data = json.dumps(test)
+    ref.set(data)    
+
+'''
+plant is likely where 2*b - g - r > 0
+
+might work here with green tags
+'''
+def segmentGreen(image):
+    # opencv image is bgr
+    mask = (2*image[:,:,1]) - image[:,:,0] - image[:,:,2]
+    
+    #showMask = cv2.resize(mask, (500,500))
+    #showImage(showMask)
+    out = np.where(mask>0, mask, 0)
+    out = np.where(out < 170, out, 0)
+    #out = np.where(2*image[1] - image[0] - image[2] > 0, image, (0,0,0))
+    return out
+
+'''
+do hough transform, filter out lines that are too far from 90deg, find interesectiosn (corners), try to count rectnagles based on this
+
+
+'''
+def detectRect(image):
+    #src = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    src = image
+    dst = cv2.Canny(src, 50, 200, None, 3)
+    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 100, None, 0, 0)
+
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            #print(theta)
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+
+    cv2.imwrite('lines.png', cdst)
+    show = cv2.resize(cdst, (500,500))
+    showImage(show)
+    show = cv2.resize(cdstP, (500,500))
+    showImage(show)
+'''
+center of mass
+just get xbar and ybar
+this should get center of mass assuming each pixel has equal mass
+'''
+def com(group):
+    rb = []
+    cb = []
+    minR = 10000
+    minC = 10000
+    maxR = -1
+    maxC = -1
+    for p in group:
+        r,c = p
+        if r < minR:
+            minR = r
+        if r > maxR:
+            maxR = r
+        if c < minC:
+            minC = c
+        if c > maxC:
+            maxC = c
+        rb.append(r)
+        cb.append(c)
+    rm = np.mean(rb)
+    cm = np.mean(cb)
+    return (rm,cm), (minR, minC), (maxR, maxC)
+
+'''
+find center of mass
+find farthest vertical pixel
+find farthest horizontal pixel
+find difference
+if diff < c * width or height accept otherwise reject
+'''
+def checkSquare(group):
+    #gImage,center = createGroupImage(group)
+    center, minP, maxP = com(group)
+    maxR, maxC = maxP
+    minR, minC = minP
+
+    rB = maxR - minR
+    cB = maxC - minC
+
+    #rc, cc = center
+    #rc = int(rc)
+    #cc = int(cc)
+
+
+    
+    # sample in a few directions, see if the range is reasonable considering the 
+
+    # use simple heuristic for now, width > height
+    return rB < cB
+def createGroupImage(group):
+    center, minP, maxP = com(group)
+    
+    minR, minC = minP
+    maxR, maxC = maxP
+    rB = maxR - minR
+    cB = maxC - minC
+    
+    out = np.zeros((rB+1, cB+1))
+    for p in group:
+        r,c = p
+        out[r-minR][c-minC] = 255
+    print('rB:', rB)
+    print('cB:', cB)
+    showImage(out)
+
+    return rB, cB
+
+def showImage(image, title='image'):
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 if __name__ == '__main__':
     main()
